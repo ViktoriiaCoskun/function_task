8c7f95f7b6e2c8817883b197f49e869e55bd2547
Viktoriia Coskun <vicky.9995022@gmail.com>
2023-01-01 18:27:32 +0100

    Add function_task.py

--- function_task.py
+++ function_task.py
@@ -1,1 +1,17 @@
+def isPalindrome(theWord): 
+    reversedWord = ""
+    theWord=theWord.lower()
+    for letter in theWord:
+        reversedWord = letter + reversedWord
+        
+    if (theWord == reversedWord):
+        return True
+    else:
+        return False
 
+if isPalindrome('Madam'):
+    print("The given word is palindrome")
+
+ 
+else:
+    print("The given word is not palindrome")
